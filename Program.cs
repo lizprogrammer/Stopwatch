12092999e335964ce4f4f49e227b8974dd773f1f
Elizabeth Rosso <elizabeth.a.rosso@gmail.com>
2018-09-11 00:27:56 +0000

    Stopwatch

--- Program.cs
+++ Program.cs
@@ -2,11 +2,43 @@
 
 namespace Stopwatch
 {
+    class Stopwatch
+    {
+        DateTime startTime;
+        DateTime stopTime;
+        DateTime duration;
+        
+        public void start()
+        {
+            this.startTime = DateTime.Now;
+            
+        }
+        public void stop()
+        {
+            this.stopTime = DateTime.Now;
+            this.duration = this.stopTime - this.startTime;
+            
+        }        
+    }
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            
+            var myWatch = new Stopwatch();
+            myWatch.start();
+            string myString = Console.ReadLine();
+            
+            while(myString != "")
+                {
+                     myString = Console.ReadLine();
+                }
+            
+            myWatch.stop();
+         
+
+            
         }
     }
 }
